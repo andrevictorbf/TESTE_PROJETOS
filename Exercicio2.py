bdb4c02aadd14a3bc6162ba7569c5c075ce1380d
andrevictorbf <andrevictor_barcia@hotmail.com>
2023-08-29 12:24:19 -0300

    Add Exercicio2.py

---
+++ Exercicio2.py
@@ -0,0 +1,10 @@
+#Programa que realiza operações matemática de Soma, subtração, multiplicação e divisão
+num1 = int(input("Digite a primeira variavel: ")) # int = conj. numeros inteiros
+num2 = int(input("Digite a segunda variavel: ")) 
+soma = num1 + num2 
+subtracao = num1 - num2
+multiplicao = num1 * num2
+divisao = num1 / num2 
+#funcao de impressão - preferi utulizar uma frase ao inves de vários comandos print para diminuir o codigo
+print(f"A soma é {soma}, a subtração é {subtracao}, a multiplicação é {multiplicao} e a divisão é {divisao}") 
+
---
+++ aula1.py
@@ -0,0 +1,3 @@
+#primeiro código no Python - 29/08/2023
+print(123)
+print (123,456, sep='-')
